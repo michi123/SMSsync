f92989365d4409044bb5f96e086034b6d0039e25
michi123 <m.kostelecky@seznam.cz>
2014-11-30 02:41:57 +0100

    -SMS to telephone (GSM) -SMS to database on server

---
+++ SMSsync2/app/src/main/java/Model/SendSMS.java
@@ -0,0 +1,16 @@
+package Model;
+
+
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Michal on 26. 11. 2014.
+ */
+public class SendSMS {
+
+
+
+}
